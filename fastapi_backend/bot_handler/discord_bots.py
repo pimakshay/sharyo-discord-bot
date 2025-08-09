514019dae4d1bfb45c77fe466fd745cbd1599781
akshayp <pimakshay@gmail.com>
2025-08-09 23:36:48 +0200

    adding slash commands

--- fastapi_backend/bot_handler/discord_bots.py
+++ fastapi_backend/bot_handler/discord_bots.py
@@ -1,13 +1,23 @@
 import os
 import discord
+from discord.ext import commands
+from discord import app_commands
 from dotenv import load_dotenv
 
 # Load environment variables from .env file
 load_dotenv("../.env")
 
-class Client(discord.Client):
+class Client(commands.Bot):
     async def on_ready(self):
         print(f"Logged in as {self.user}!")
+
+        try:
+
+            synced = await self.tree.sync(guild=GUILD_ID)
+            print(f"Synced {len(synced)} command(s)")
+
+        except Exception as e:
+            print(e)
 
     async def on_message(self, message):
         # Ignore messages sent by the bot itself
@@ -41,6 +51,18 @@ class Client(discord.Client):
 intents = discord.Intents.default()
 intents.message_content = True
 
+GUILD_ID = discord.Object(id=os.getenv("SERVER_GUILD_ID"))
+
+
 # Create and run the client
-client = Client(intents=intents)
+client = Client(command_prefix="!", intents=intents) # ! was used to call functions in discord. Now, it is replaced with / command, but initiated with !
+
+@client.tree.command(name="hallo", description="Say Hello!", guild=GUILD_ID)
+async def say_hello(interaction: discord.Interaction):
+    await interaction.response.send_message(f"Hi, {interaction.user.mention}!")
+
+@client.tree.command(name="drueck", description="I print what you say!", guild=GUILD_ID)
+async def druecker(interaction: discord.Interaction, printer: str, year: int):
+    await interaction.response.send_message(f"Hi, {interaction.user.mention}!. You said: {printer} in {year}!")
+
 client.run(os.getenv("DISCORD_TOKEN"))
